7d6a770a714cc7c3344f3e0eaf6f9e234e2499ba
diceky <daisuke4yukita@gmail.com>
2024-07-24 05:10:42 -0500

    import fetch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import fetch from "node-fetch";
 
 const app = express();
 app.use(bodyParser.json());
